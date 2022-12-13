c1ebdc8f68f256c2c79c0162ff417c5f32ff663d
Mehdi Ayache <mehdi@noie.com>
2022-12-13 17:41:56 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <weather-view />
+    <search-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import WeatherView from "@/components/WeatherView";
+import SearchView from "@/components/SearchView";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    WeatherView,
+    SearchView,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/WeatherView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-[500px] h-[666px]">
+    <img src="@/assets/images/placeholder.jpg" alt="" />
+  </div>
+</template>
+<script>
+export default {};
+</script>
