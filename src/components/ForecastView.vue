a3261c60b97399c69e1f3b432dfa9ebf0a07e668
Mehdi Ayache <mehdi@noie.com>
2022-12-14 14:13:48 +0100

    :lipstick: search begin

--- src/components/ForecastView.vue
+++ src/components/ForecastView.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="rounded-3xl bg-night-dark h-[620px] w-[600px] -ml-20 text-white">
     <forecast-tab class="pl-36 pr-12" />
+    <search-tab />
   </div>
 </template>
 <script>
 import ForecastTab from "@/components/forecast/ForecastTab.vue";
+import SearchTab from "@/components/forecast/SearchTab.vue";
 export default {
-  components: { ForecastTab },
+  components: { ForecastTab, SearchTab },
 };
 </script>
---
+++ src/components/forecast/SearchTab.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="relative mt-16 max-w-[415px] ml-36">
+    <input
+      type="text"
+      ref="search"
+      class="linear-background w-[415px] h-[53px] rounded-xl pl-12 peer"
+      @click="triggeredInput = true"
+      @blur="triggeredInput = false"
+    />
+
+    <img
+      @click="triggeredInput = true"
+      src="@/assets/icons/location.svg"
+      class="cursor-pointer w-8 h-8 absolute top-1/2 -translate-y-1/2 transition-all"
+      :class="{ 'left-2': triggeredInput, 'left-[82px]': !triggeredInput }"
+    />
+    <div
+      @click="triggeredInput = true"
+      class="absolute cursor-pointer -translate-x-1/2 text-[1.25rem] -translate-y-1/2 transition-all font-semibold"
+      :class="{
+        'left-[86px] top-[-20px] ': triggeredInput,
+        'left-1/2  top-1/2 ': !triggeredInput,
+      }"
+    >
+      Change Location
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  watch: {
+    triggeredInput(value) {
+      if (value) {
+        this.$refs["search"].focus();
+      } else {
+        this.$refs["search"].blur();
+      }
+    },
+  },
+  data() {
+    return {
+      triggeredInput: false,
+    };
+  },
+};
+</script>
